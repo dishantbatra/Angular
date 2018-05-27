49696d9746272eda768cc91215efee0a65552a84
DishantBatra <dishant.batra@nagarro.com>
2018-05-27 23:24:55 +0530

    Chapter 8

--- app/store/store.component.ts
+++ app/store/store.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { Product } from "../model/product.model";
 import { ProductRepository } from "../model/product.repository";
+import { Cart } from "../model/cart.model";
+import { Router } from "@angular/router";
 @Component({
   selector: "store",
   moduleId: module.id,
@@ -10,7 +12,11 @@ export class StoreComponent {
   public selectedCategory = null;
   public productsPerPage = 4;
   public selectedPage = 1;
-  constructor(private repository: ProductRepository) {}
+  constructor(
+    private repository: ProductRepository,
+    private cart: Cart,
+    private router: Router
+  ) {}
   get products(): Product[] {
     let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
     debugger;
@@ -41,5 +47,10 @@ export class StoreComponent {
     )
       .fill(0)
       .map((x, i) => i + 1);
+  }
+
+  addProductToCart(product: Product) {
+    this.cart.addLine(product);
+    this.router.navigateByUrl("/cart");
   }
 }
